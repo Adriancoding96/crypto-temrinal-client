ab08d7ac22322ce42be358822dd2e9dcd696214e
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-06 17:16:55 +0000

    **Added** test method to verify file creation

--- src/test/java/com/adrian/bank/FileSystemTests.java
+++ src/test/java/com/adrian/bank/FileSystemTests.java
@@ -2,6 +2,7 @@ package com.adrian.bank;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
@@ -52,6 +53,13 @@ public class FileSystemTests {
       fileSystem.readFileContent(TEST_DIRECTORY + "notimplementedyet.java"),
       "Sould throw IOException if file not found"
       );
+  }
+
+  @Test
+  void testCreateFile() throws IOException {
+    fileSystem.createFile(TEST_DIRECTORY, TEST_FILE);
+    File file = new File(TEST_DIRECTORY + TEST_FILE);
+    assertTrue(file.exists() && file.isFile(), "File should be created");
   }
   
 }

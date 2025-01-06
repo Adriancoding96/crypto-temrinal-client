9bb5d64405b30a1e0d30922df53e2e651b9a46d1
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-06 12:19:09 +0000

    **Added** abstract account model

--- src/main/java/com/adrian/bank/model/Account.java
+++ src/main/java/com/adrian/bank/model/Account.java
@@ -1,5 +1,35 @@
 package com.adrian.bank.model;
 
+import java.util.List;
+
 public abstract class Account {
+ 
+  private Long id;
   
+  private List<Transaction> transactions;
+
+  public Account() {
+
+  }
+
+  public Account(Long id, List<Transaction> transactions) {
+    this.id = id;
+    this.transactions = transactions;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public List<Transaction> getTransactions() {
+    return transactions;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public void setTransactions(List<Transaction> transactions) {
+    this.transactions = transactions;
+  }
 }
